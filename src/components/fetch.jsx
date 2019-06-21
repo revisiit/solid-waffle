1dee1be7b5771d20b98ba7eb5f18d2778d679d23
unknown <aravindvitkrishnan@gmail.com>
2019-06-21 14:50:10 +0530

    added loader

--- src/components/fetch.jsx
+++ src/components/fetch.jsx
@@ -1,27 +1,43 @@
 import React from 'react'
+import Loader from '../images/loader.svg'
+import Image from '../images/logo-visit.png'
 
 class Fetch extends React.Component {
   constructor() {
     super()
     this.state = {
       data: [],
+      isloading: true,
     }
   }
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
+  componentWillMount() {
+    fetch('http://127.0.0.1:3000/api/v1/category/all')
       .then(res => res.json())
       .then(output => {
-        this.setState({ data: output })
-        console.log(this.state.data)
+        this.setState({ data: output, isloading: false })
+        console.log(output)
       })
   }
 
   render() {
+    if (this.state.isloading) {
+      return (
+        <div>
+          <p>loading....</p>
+          <img src={Loader} />
+        </div>
+      )
+    }
+
     return (
       <div>
+        {this.state.data.map(item => (
+          <h4 key={item.key}>{item._id}</h4>
+        ))}
         <div>
           <p>bravo </p>
+          <img src={Image} />
         </div>
       </div>
     )
