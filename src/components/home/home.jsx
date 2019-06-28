f9f411018887aef9aa9265132d435fcf8d813dcd
unknown <aravindvitkrishnan@gmail.com>
2019-06-28 23:54:13 +0530

    Added home where all packages and categories are fetched

---
+++ src/components/home/home.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import Loader from '../common/loader'
+import Image from '../images/logo-visit.png'
+import Category from '../category_id'
+import Package from '../package_id/'
+import { Switch, BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Error from '../common/error'
+
+class Fetch extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      pkg: [],
+      isloading: true,
+      error: null,
+      category: [],
+    }
+  }
+
+  componentWillMount() {
+    fetch('http://127.0.0.1:3000/api/v1/package/all')
+      .then(res => res.json())
+      .then(pkg_output => {
+        this.setState({ pkg: pkg_output, isloading: false })
+        console.log(pkg_output)
+      })
+      .catch(error => this.setState({ error, isloading: false }))
+
+    fetch('http://127.0.0.1:3000/api/v1/category/all')
+      .then(res => res.json())
+      .then(category_output => {
+        this.setState({ category: category_output, isloading: false })
+        console.log(category_output)
+      })
+      .catch(error => this.setState({ error, isloading: false }))
+  }
+
+  render() {
+    if (this.state.isloading) {
+      return <Loader />
+    }
+    if (this.state.error) {
+      return <Error />
+    }
+    const { pkg } = this.state
+    const { category } = this.state
+    return (
+      <div>
+        {pkg.map(pkg_item => (
+          <div key={pkg_item._id}>
+            {' '}
+            <h2>
+              <Link to={`/package_id/${pkg_item._id}`}>{pkg_item.name}</Link>
+            </h2>
+            <h3>{pkg_item.description}</h3>
+          </div>
+        ))}
+        {category.map(category_item => (
+          <div key={category_item._id}>
+            {' '}
+            <h2>
+              <Link to={`/category_id/${category_item._id}`}>
+                {category_item.name}
+              </Link>
+            </h2>
+          </div>
+        ))}
+      </div>
+    )
+  }
+}
+
+export default Fetch
