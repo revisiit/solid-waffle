69294d02ed00bf8eed165040a85fe98de1e78daa
unknown <aravindvitkrishnan@gmail.com>
2019-06-29 00:02:47 +0530

    Added loader and error in common folder

---
+++ src/components/common/error.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Error extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Something is wrong</p>
+      </div>
+    )
+  }
+}
+
+export default Error
