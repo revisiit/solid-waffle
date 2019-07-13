043b240dc828cfe60f1ad42b1158eca51e657c4c
unknown <aravindvitkrishnan@gmail.com>
2019-07-14 01:17:01 +0530

    checking post

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Router from './components/router'
+import Login from './components/login'
 
 const targetEl = document.getElementById('root')
-ReactDOM.render(<Router />, targetEl)
+ReactDOM.render(<Login />, targetEl)
